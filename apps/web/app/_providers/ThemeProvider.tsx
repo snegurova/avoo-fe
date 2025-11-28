688feafe6b48c3f9690216c585361c3e4bb69c50
Mykhailo Kuzich <timmkedth@gmail.com>
2025-11-28 19:41:26 +0100

    avoo-274: Changed theme import, theme provider

--- apps/web/app/_providers/ThemeProvider.tsx
+++ apps/web/app/_providers/ThemeProvider.tsx
@@ -1,16 +1,12 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ReactNode } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../../theme';
+import theme from '@/../theme';
 
 export function MUIThemeProvider({ children }: { children: ReactNode }) {
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </AppRouterCacheProvider>
   );
 }
