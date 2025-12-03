309639cf0616a942b5c190ea893daf8685a05ef7
Erpokk <maimav56@gmail.com>
2025-12-03 10:43:43 +0100

    feat(avoo-216): add loader for avatar

--- apps/web/app/_components/Avatar/AvatarUpload.tsx
+++ apps/web/app/_components/Avatar/AvatarUpload.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import React from 'react';
-import { Avatar, ButtonBase, CircularProgress } from '@mui/material';
+import { Avatar, ButtonBase } from '@mui/material';
 import { utilsHooks } from '@/_utils/utilsHooks';
 import { AVATAR_DOWNLOAD_FORMATS } from '@/_constants/files';
+import AvatarLoader from '../AvatarLoader/AvatarLoader';
 
-enum AvatarSize {
+export enum AvatarSize {
   SMALL = 64,
   MEDIUM = 80,
   LARGE = 96,
@@ -20,8 +21,6 @@ type Props = {
 
 export const AvatarUpload = (props: Props) => {
   const { imageUri, onImageSelected, isLoading, size = AvatarSize.LARGE } = props;
-
-  const circularProgressSize = size - 20;
 
   const handleChoosePhoto = (event: React.ChangeEvent<HTMLInputElement>) => {
     utilsHooks.handleFileChange(event, onImageSelected);
@@ -40,11 +39,7 @@ export const AvatarUpload = (props: Props) => {
         sx={{ width: size, height: size }}
         src={imageUri ?? undefined}
       />
-      {isLoading && (
-        <span className='absolute inset-0 flex items-center justify-center'>
-          <CircularProgress size={circularProgressSize} color='primary' />
-        </span>
-      )}
+      {isLoading && <AvatarLoader size={size} />}
       <input
         type='file'
         accept={AVATAR_DOWNLOAD_FORMATS}
