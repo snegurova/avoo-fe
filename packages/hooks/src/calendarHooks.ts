78d6672d68b3c0539a6a851be1cffaecf097129b
Mykhailo Kuzich <timmkedth@gmail.com>
2025-12-06 15:43:13 +0100

    avoo-262: Added momo for params in get calanedar

--- packages/hooks/src/calendarHooks.ts
+++ packages/hooks/src/calendarHooks.ts
@@ -5,12 +5,32 @@ import { PrivateCalendarQueryParams } from '@avoo/axios/types/apiTypes';
 import { BaseResponse, GetCalendarResponse } from '@avoo/axios/types/apiTypes';
 import { apiStatus } from '@avoo/hooks/types/apiTypes';
 import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 
 export const calendarHooks = {
-  useGetCalendar: (params: PrivateCalendarQueryParams) => {
+  useGetCalendar: ({
+    masterId,
+    view,
+    rangeFromDate,
+    rangeToDate,
+    serviceId,
+    combinationId,
+  }: PrivateCalendarQueryParams) => {
+    const memoParams = useMemo<PrivateCalendarQueryParams>(
+      () => ({
+        masterId,
+        view,
+        rangeFromDate,
+        rangeToDate,
+        serviceId,
+        combinationId,
+      }),
+      [masterId, view, rangeFromDate, rangeToDate, serviceId, combinationId],
+    );
+
     const { data: calendarData, isPending } = useQuery<BaseResponse<GetCalendarResponse>, Error>({
-      queryKey: ['calendar', params],
-      queryFn: () => calendarApi.getCalendar(params),
+      queryKey: ['calendar', memoParams],
+      queryFn: () => calendarApi.getCalendar(memoParams),
     });
 
     utils.useSetPendingApi(isPending);
