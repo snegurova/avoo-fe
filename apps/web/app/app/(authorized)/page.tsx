c1ac3bca6e8df21afa8e1bfcdfdaf65674e0dbc6
innasnegurova <snegurova@gmail.com>
2025-12-08 00:06:01 +0100

    feat: AVOO-209 add colors tokens and set mui theme and tailwind  with design tokens

--- apps/web/app/app/(authorized)/page.tsx
+++ apps/web/app/app/(authorized)/page.tsx
@@ -1,5 +1,12 @@
 'use client';
+import { Chip } from '@mui/material';
 
 export default function DashboardPage() {
-  return <div className=''>DashboardPage</div>;
+  return (
+    <>
+      <div className=''>DashboardPage</div>
+      <Chip label='Pending' color='pending' />
+      <Chip label='Out of Schedule' color='outOfSchedule' />
+    </>
+  );
 }
