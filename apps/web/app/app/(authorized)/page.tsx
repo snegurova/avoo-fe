223b5ab51e037aeaab6f134d16094660eeeaff79
innasnegurova <snegurova@gmail.com>
2025-12-08 00:13:35 +0100

    feat: change chip size

--- apps/web/app/app/(authorized)/page.tsx
+++ apps/web/app/app/(authorized)/page.tsx
@@ -5,8 +5,8 @@ export default function DashboardPage() {
   return (
     <>
       <div className=''>DashboardPage</div>
-      <Chip label='Pending' color='pending' />
-      <Chip label='Out of Schedule' color='outOfSchedule' />
+      <Chip label='Pending' color='pending' size='small' />
+      <Chip label='Out of Schedule' color='outOfSchedule' size='small' />
     </>
   );
 }
