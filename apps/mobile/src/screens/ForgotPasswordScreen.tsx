d591ed9ef0ae6fbd13ef1406063bc110fbb4e1ea
Erpokk <maimav56@gmail.com>
2025-12-21 20:59:44 +0100

    avoo-303: fix correect layout usage

--- apps/mobile/src/screens/ForgotPasswordScreen.tsx
+++ apps/mobile/src/screens/ForgotPasswordScreen.tsx
@@ -9,7 +9,7 @@ export default function ForgotPasswordScreen(props: Props) {
   const { navigation } = props;
 
   return (
-    <Layout centerContent={true}>
+    <Layout centerContent={true} isHeaderHidden={true}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View className='w-full'>
           <Text className='text-3xl font-bold text-center mb-2'>Forgot Password</Text>
