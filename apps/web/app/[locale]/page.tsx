25c323d9f6f1853c165a43e844fab7e85629bdf2
Hrebynakha Anatolii <hrebynakha@gmail.com>
2026-02-26 17:43:26 +0200

    upd page link

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,20 +1,20 @@
 'use client';
+import Link from 'next/link';
 import { FormattedMessage } from 'react-intl';
 import { messages } from '@avoo/intl/messages/public/home/topSection';
 import { AppRoutes } from '@/_routes/routes';
-import { LocalizedLink } from '@/_components/LocalizedLink/LocalizedLink';
 import { localizationHooks } from '@/_hooks/localizationHooks';
 
 export default function HomePage() {
   return (
     <div className='container flex flex-col items-center gap-10'>
       <FormattedMessage {...messages.title} />
-      <LocalizedLink
+      <Link
         href={localizationHooks.useWithLocale(AppRoutes.Home)}
         className='bg-primary-500 text-white py-2.5 px-4 rounded-2xl'
       >
         <FormattedMessage {...messages.ctaButton} />
-      </LocalizedLink>
+      </Link>
     </div>
   );
 }
