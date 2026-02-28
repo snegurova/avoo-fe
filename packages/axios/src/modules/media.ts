48678e56a0b668f60104b83b2626137570b1f27f
innasnegurova <snegurova@gmail.com>
2026-02-28 13:36:50 +0100

    fix: fileHooks

--- packages/axios/src/modules/media.ts
+++ packages/axios/src/modules/media.ts
@@ -1,25 +1,13 @@
 import { BaseResponse, UploadMediaRequest, UploadMediaResponse } from '@avoo/axios/types/apiTypes';
 import { apiClient } from '@avoo/axios/src/apiClient';
+import { formDataUtils } from '../../utils/formDataUtils';
 
 const MEDIA_ENDPOINT = '/media';
 
 export const mediaApi = {
   async uploadMedia(data: UploadMediaRequest) {
-    const formData = new FormData();
+    const formData = formDataUtils.getFileFormData(data);
 
-    const file = data.file;
-
-    if (typeof Blob !== 'undefined' && file instanceof Blob) {
-      if (typeof File !== 'undefined' && file instanceof File) {
-        formData.append('file', file, file.name);
-      } else {
-        formData.append('file', file);
-      }
-    } else {
-      formData.append('file', file as unknown as Blob);
-    }
-
-    formData.append('type', data.type);
     const res = await apiClient.post<BaseResponse<UploadMediaResponse>>(MEDIA_ENDPOINT, formData, {
       headers: { 'Content-Type': 'multipart/form-data' },
     });
