bfba8abf2841355056e7f5917bba2d527734638e
Mykhailo Kuzich <timmkedth@gmail.com>
2026-03-07 22:35:13 +0100

    avoo-463: Added security settings and Profile settings pages

---
+++ apps/web/app/[locale]/app/(authorized)/profile/security-settings/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { Typography } from '@mui/material';
+
+import AppWrapper from '@/_components/AppWrapper/AppWrapper';
+
+import { FormattedMessage } from 'react-intl';
+import { messages } from '@avoo/intl/messages/private/profile/profile';
+
+export default function SecuritySettingsPage() {
+  return (
+    <AppWrapper>
+      <div className='p-4 flex justify-between items-center'>
+        <Typography component='h1' variant='h1'>
+          <FormattedMessage {...messages.securitySettings} />
+        </Typography>
+      </div>
+    </AppWrapper>
+  );
+}
