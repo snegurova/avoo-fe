e66af4ae7e14741ea5dc8df4e08dd1b178d8d440
Natalia Kucheriava <nkucheryava@gmail.com>
2026-03-12 16:28:32 +0100

    fix in export of date-format

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,13 +1,5 @@
 export * from './calendar';
 export * from './dateFormats';
-export {
-  DATE_DISPLAY_FORMAT,
-  DATE_PICKER_FORMAT,
-  DATE_TIME_FORMAT,
-  DATE_TIME_PICKER_FORMAT,
-  TIME_FORMAT,
-  VALUE_DATE_FORMAT,
-} from './dateFormats';
 export type { LanguageCode } from './languages';
 export { LANGUAGE_NAMES, VALID_LANGUAGE_CODES } from './languages';
 export type { PhoneCodeOption } from './phoneCodes';
